567e1ea28dbf622f8861da5bc71839da32ce0582
Niklas Pfister <accounts@omikron.pw>
2019-11-12 14:26:56 +0100

    Added FieldBuilder to build field queries

--- centreon_sdk/builder/field_builder.py
+++ centreon_sdk/builder/field_builder.py
@@ -1,63 +1,24 @@
-class FieldBuilder:
-    """This class is used to build field queries. Specify all fields, you want to receive.
+from centreon_sdk.util import method_utils
 
-    :param id_unique: Id of Host
-    :param name: Name of host
-    :param alias: Alias of host
-    :param address: Address of host (domain name or ip)
-    :param state: State of host (UP = 0, DOWN = 2, UNREA = 3)
-    :param state_type: State type of host (SOFT = 0, HARD = 1)
-    :param output: Plugin output - state message
-    :param max_check_attempts: Maximum check attempts
-    :param check_attempt: Current attempts
-    :param last_check: Last check time
-    :param last_state_change: Last time the state changed
-    :param last_hard_state_changed: Last time the state changed in hard type
-    :param acknowledged: Acknowledged flag
-    :param instance: Name of the instance which checks this host
-    :param instance_id: ID of the instance which checks this host
-    :param critically: Critically flag for this host
-    :param passive_checks: 
-    :param active_checks:
-    :param notify:
-    :param action_url:
-    :param notes_url:
-    :param notes:
-    :param icon_image:
-    :param icon_image_alt:
-    :param scheduled_downtime_depth:
-    :param flapping:
+
+class FieldBuilder:
+    """This class is used to build field queries. Specify all fields as kwargs, you want to receive.
+
+    Example:
+        ```FieldBuilder(id=True, output=True).build()```
+
+        Returns the str: ```id,output```
     """
-    def __init__(self, *, id_unique=False, name=False, alias=False, address=False, state=False, state_type=False,
-                 output=False, max_check_attempts=False, check_attempt=False, last_check=False, last_state_change=False,
-                 last_hard_state_changed=False, acknowledged=False, instance=False, instance_id=False, critically=False,
-                 passive_checks=False, active_checks=False, notify=False, action_url=False, notes_url=False, notes=False,
-                 icon_image=False, icon_image_alt=False, scheduled_downtime_depth=False, flapping=False):
-        self.id_unique = id_unique
-        self.name = name
-        self.alias = alias
-        self.address = address
-        self.state = state
-        self.state_type = state_type
-        self.output = output
-        self.max_check_attempts = max_check_attempts
-        self.check_attempt = check_attempt
-        self.last_check = last_check
-        self.last_state_change = last_state_change
-        self.last_hard_state_changed = last_hard_state_changed
-        self.acknowledged = acknowledged
-        self.instance = instance
-        self.instance_id = instance_id
-        self.critically = critically
-        self.passive_checks = passive_checks
-        self.active_checks = active_checks
-        self.notify = notify
-        self.action_url = action_url
-        self.notes_url = notes_url
-        self.notes = notes
-        self.icon_image = icon_image
-        self.icon_image_alt = icon_image_alt
-        self.scheduled_downtime_depth = scheduled_downtime_depth
-        self.flapping = flapping
+    def __init__(self, **kwargs):
+        self.args = {}
+        for item in kwargs:
+            self.args[item] = kwargs[item]
 
     def build(self):
+        """This method is used to build the field query string
+
+        :return: Returns the field query string
+        :rtype: list
+        """
+        var_dict = method_utils.resubstitute_vars(self.args)
+        return ",".join([item for item in var_dict if var_dict[item]])
