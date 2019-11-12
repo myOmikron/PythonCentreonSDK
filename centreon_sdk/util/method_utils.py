75d243f6cde4a289faa1a00fa97b9b34e6d207d2
Niklas Pfister <accounts@omikron.pw>
2019-11-12 14:25:57 +0100

    Added pack_locals, substitute_vars and resubstiture_vars in util.method_utils

--- centreon_sdk/util/method_utils.py
+++ centreon_sdk/util/method_utils.py
@@ -1,15 +1,64 @@
+import json
+from json import JSONEncoder
 
-def pack_locals(local_dict):
+
+class MyEncoder(JSONEncoder):
+    def default(self, o):
+        if isinstance(o, object):
+            return o.__dict__
+        else:
+            return json.JSONEncoder.default(self, o)
+
+
+def pack_locals(kwargs):
     """This method is used to pack the locals to another dict
 
-    :param local_dict: locals to process
-    :type local_dict: dict
+    :param kwargs: locals to process
+    :type kwargs: dict
 
     :return: Returns dict with variables from local_dict
     :rtype: dict
     """
-    ret = {}
-    for item in local_dict:
-        if item != "self" or not local_dict[item]:
-            ret[item] = local_dict[item]
-    return ret
+    ret_dict = {}
+    for item in kwargs:
+        if kwargs[item] and not item == "self":
+            if isinstance(kwargs[item], list):
+                kwargs[item] = MyEncoder().encode(kwargs[item])
+            ret_dict[item] = kwargs[item]
+    return ret_dict
+
+
+def substitute_vars(var_dict):
+    """This method is used to substitute the names of the variables of own classes
+
+    :param var_dict: Dict, which holds the variables to process
+    :type var_dict: dict
+
+    :return: New dict, which holds all variable names, changed and unchanged
+    :rtype: dict
+    """
+    ret_dict = {}
+    for item in var_dict:
+        if item == "id":
+            ret_dict["id_unique"] = var_dict["id"]
+        else:
+            ret_dict[item] = var_dict[item]
+    return ret_dict
+
+
+def resubstitute_vars(var_dict):
+    """This method is sued to resubstitute the names of the variables of own classes
+
+    :param var_dict: Dict, which holds the variables to process
+    :type var_dict: dict
+
+    :return: Returns new dict, which holds all variables names, changed and unchanged
+    :rtype: dict
+    """
+    ret_dict = {}
+    for item in var_dict:
+        if item == "id_unique":
+            ret_dict["id"] = var_dict["id_unique"]
+        else:
+            ret_dict[item] = var_dict[item]
+    return ret_dict
