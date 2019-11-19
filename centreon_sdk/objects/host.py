b15de7f60393365940309639abd8a1c9065b542b
Niklas Pfister <accounts@omikron.pw>
2019-11-19 09:12:23 +0100

    Added documentation for host object

--- centreon_sdk/objects/host.py
+++ centreon_sdk/objects/host.py
@@ -1,5 +1,17 @@
-
 class Host:
+    """This class represents a host object
+
+    :param name: Name of the host
+    :type name: str
+    :param alias: Alias of the host
+    :type alias: str
+    :param address: Address of the host
+    :type address: str
+    :param activate: Is the host activated?
+    :type activate: bool
+    :param id_unique: Id of the host
+    :type id_unique: int
+    """
     def __init__(self, *, name, alias, address, activate, id_unique):
         self.name = name
         self.alias = alias
