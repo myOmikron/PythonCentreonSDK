9f66afd845ce50e0cad082fb009c395fbc72824a
Niklas Pfister <accounts@omikron.pw>
2019-11-19 09:13:40 +0100

    Added documentation of macro

--- centreon_sdk/objects/macro.py
+++ centreon_sdk/objects/macro.py
@@ -1,4 +1,17 @@
 class Macro:
+    """This class represents a macro
+
+    :param macro_name: Name of the macro
+    :type macro_name: str
+    :param macro_value: Value of the macro
+    :type macro_name: str
+    :param is_password: Is the macro an password?
+    :type is_password: bool
+    :param description: Description of the macro
+    :type description: str
+    :param source: Source the macro came from
+    :type source: str
+    """
     def __init__(self, *, macro_name, macro_value, is_password, description, source):
         self.macro_name = macro_name
         self.macro_value = macro_value
