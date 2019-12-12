6bdda3db53f70ff0e40316b3036cfbc9689eab5c
Niklas Pfister <accounts@omikron.pw>
2019-12-12 16:50:37 +0100

    Updated pypi to version 0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setuptools.setup(
     name="centreon-sdk",
-    version="0.1.2",
+    version="0.1.3",
     author="Niklas Pfister",
     author_email="contact@omikron.pw",
     description="A SDK for python to communicate with Centreon",
